2b47d55e5850a04ef5472ab04779f673683f0651
svenpai8 <svan1001@live.mark.se>
2018-08-30 14:53:59 +0200

    2nd commit

---
+++ Exempel/main.cpp
@@ -0,0 +1,15 @@
+// exempel 1.3, sid 18
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	// "-tecknet används för att
+	// visa var strängen börjar
+	// och var den slutar
+	cout << "\"To be or no to be, that is the question\"";
+	
+	
+	
+	return 0;
+}
